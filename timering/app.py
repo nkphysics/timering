7a431ad66063958d7e1cb69f38b3a8d3a05612ec
Tsar Bomba Nick <njkuechel@protonmail.com>
2023-05-29 18:01:26 -0600

    Added filtering for error bars on gaussian fit error bars

--- timering/app.py
+++ timering/app.py
@@ -17,44 +17,6 @@ p.add_argument("--database",
 
 pargs = p.parse_args()
 
-if "show_df" not in st.session_state:
-    st.session_state.show_df = "Off"
-
-with st.sidebar:
-    st.markdown("# Timering")
-    dbpath = st.text_input("Local database path", value=pargs.database)
-    dbpath = pathlib.Path(dbpath).resolve()
-    plottype = st.radio("Timing Evolution Plot Type", ["Line", "Scatter"])
-    show_df = st.radio(
-        "Show Data Table",
-        ["Off", "On"],
-    )
-    with st.expander("Timing Evolution Filters"):
-        st.markdown("Interval Modes:")
-        set1, set2 = st.columns(2)
-        with set1:
-            xteintmode = st.selectbox("XTE",
-                                      options=["all", "full", "gti"]
-                                      )
-        with set2:
-            nicerintmode = st.selectbox("NICER",
-                                        options=["all", "full", "gti"]
-                                        )
-        st.markdown(r"Error ($hz$):")
-        set3, set4 = st.columns(2)
-        with set3:
-            minerr = st.text_input("Min", value=0.0)
-        with set4:
-            maxerr = st.text_input("Max", value=1.0)
-
-
-st.session_state.show_df = show_df
-
-con = sqlite3.connect(dbpath)
-table_in = pd.read_sql_query("SELECT * FROM nu_results", con)
-table_in["TIME"] = pd.to_datetime(table_in["TIME"])
-table_in = table_in.sort_values(by="TIME")
-
 
 def querymission(mission):
     mtable = table_in.loc[table_in["MISSION"] == mission.upper()]
@@ -77,13 +39,58 @@ def filtermin(table, column, minbound):
     return table
 
 
-nitable = querymission("NICER")
-nitable = filter_intmode(nitable, nicerintmode)
-xtetable = querymission("XTE")
-xtetable = filter_intmode(xtetable, xteintmode)
-table_in = pd.merge(xtetable, nitable, how="outer")
-table_in = filtermax(table_in, "NU_ERR", maxerr)
-table_in = filtermin(table_in, "NU_ERR", minerr)
+if "show_df" not in st.session_state:
+    st.session_state.show_df = "Off"
+
+with st.sidebar:
+    st.markdown("# Timering")
+    dbpath = st.text_input("Local database path", value=pargs.database)
+    dbpath = pathlib.Path(dbpath).resolve()
+    plottype = st.radio("Timing Evolution Plot Type", ["Line", "Scatter"])
+    show_df = st.radio(
+        "Show Data Table",
+        ["Off", "On"],
+    )
+
+con = sqlite3.connect(dbpath)
+table_in = pd.read_sql_query("SELECT * FROM nu_results", con)
+table_in["TIME"] = pd.to_datetime(table_in["TIME"])
+table_in = table_in.sort_values(by="TIME")
+with st.sidebar:
+    with st.expander("Timing Evolution Filters"):
+        st.markdown("Interval Modes:")
+        set1, set2 = st.columns(2)
+        with set1:
+            xteintmode = st.selectbox("XTE",
+                                      options=["all", "full", "gti"]
+                                      )
+        with set2:
+            nicerintmode = st.selectbox("NICER",
+                                        options=["all", "full", "gti"]
+                                        )
+        nitable = querymission("NICER")
+        nitable = filter_intmode(nitable, nicerintmode)
+        xtetable = querymission("XTE")
+        xtetable = filter_intmode(xtetable, xteintmode)
+        table_in = pd.merge(xtetable, nitable, how="outer")
+        st.markdown(r"$\nu$ Error (hz)")
+        set3, set4 = st.columns(2)
+        with set3:
+            minerr = st.text_input("Min Error", value=0.0)
+        with set4:
+            maxerr = st.text_input("Max Error", value=1.0)
+        table_in = filtermax(table_in, "NU_ERR", maxerr)
+        table_in = filtermin(table_in, "NU_ERR", minerr)
+        st.markdown(r"$\nu$ Gaussian Fit Error (hz)")
+        set5, set6 = st.columns(2)
+        with set5:
+            mingerr = st.text_input("Min Gaussian Error", value=0.0)
+        with set6:
+            maxgerr = st.text_input("Max Gaussian Error", value=1.0)
+        table_in = filtermax(table_in, "G_NU_ERR", maxgerr)
+        table_in = filtermin(table_in, "G_NU_ERR", mingerr)
+
+st.session_state.show_df = show_df
 
 
 def tevo_plot(table, plottype):
